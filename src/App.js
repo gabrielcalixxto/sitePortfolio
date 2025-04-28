5d187bc87bc6bd0ea2d0f66219366551344ae35d
Gabriel Calixto <gabrielcalixtosoares@gmail.com>
2025-04-27 21:17:23 -0300

    Start website project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header className="container-header">
+        <h1>Inicio</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
