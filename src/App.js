6db51f4b69ee78bbb9589b3c123c10b6f4642303
Gabriel Calixto <gabrielcalixtosoares@gmail.com>
2025-05-03 19:35:08 -0300

    done html initial content

--- src/App.js
+++ src/App.js
@@ -2,10 +2,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      <header className="container-header">
-        <h1>Inicio</h1>
+    <div className="body">
+      <header className="navbar">
+        <div className="logo">Meu Logo</div>
+        <nav>
+          <ul className="nav-links">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#sobre-mim">Sobre Mim</a></li>
+            <li><a href="#projetos">Projetos</a></li>
+            <li><a href="#contato">Contato</a></li>
+          </ul>
+        </nav>
       </header>
+      <h1>Gabriel Calixto</h1>
+      <span id="sub-title">Desenvolvedor Full Stack | Foco em soluções ágeis e escaláveis</span>
+      <p>A tecnologia serve como ponte para impulsionar a liberdade, a eficiência e a evolução das empresas</p>
+      <ul id="title-links"></ul>
+      <li>Github</li>
+      <li>HackerRank</li>
+      <li>Linkedin</li>
+      <li>Currículo</li>
+      <p>Links:</p>
+      <span id="title-projects">Projetos Principais</span>
+      <div id = "project-exibition">Box dos projetos</div>
+      <div className="whatsapp-box">
+        <a href="https://wa.me/SEU_NUMERO" target="_blank" rel="noopener noreferrer">
+          <img src="whatsapp-logo.png" alt="Ícone Whatsapp com seu link" />
+        </a>
+      </div>
     </div>
   );
 }
